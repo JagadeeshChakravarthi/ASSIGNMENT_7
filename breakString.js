ed13259cb6d64f41368a757423585927b58128ea
JagadeeshChakravarthi <110399596+JagadeeshChakravarthi@users.noreply.github.com>
2023-06-07 08:15:04 +0530

    Create breakString.js

---
+++ breakString.js
@@ -0,0 +1,8 @@
+// How can you break a string to a newline in Javascript ?
+  
+// Using the " \n " escape sequence: You can use the \n escape sequence to represent a newline character in a string.
+
+// proof:
+const string = "This is the first line.This is the second line.";
+const lines = string.split("\n");
+
